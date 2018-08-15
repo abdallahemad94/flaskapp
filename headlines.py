7c33cdde75545d28851400a3921c462832d21de8
Abdallah Emad <AbdallahEmad94@hotmail.com>
2018-08-15 04:42:23 +0200

    update pipenv and procfile

--- headlines.py
+++ headlines.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import feedparser
 
-APP = Flask(__name__)
+app = Flask(__name__)
 
 rss_feed = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             'cnn': 'http://rss.cnn.com/rss/edition.rss',
@@ -9,8 +9,8 @@ rss_feed = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 
-@APP.route('/')
-@APP.route('/<publication>')
+@app.route('/')
+@app.route('/<publication>')
 def get_news(publication='bbc'):
     """docstring"""
     feed = feedparser.parse(rss_feed[publication])
@@ -31,4 +31,4 @@ def get_news(publication='bbc'):
         )
 
 if __name__ == '__main__':
-    APP.run(port=5000, debug=True)
+    app.run(port=5000, debug=True)
