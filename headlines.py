fd42855be9bed64d59c2fdd34fd420451ec0fc4c
Abdallah Emad <AbdallahEmad94@hotmail.com>
2018-08-15 05:48:10 +0200

    update .py file and use Jinja template

--- headlines.py
+++ headlines.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 import feedparser
 
 app = Flask(__name__)
@@ -14,21 +15,7 @@ rss_feed = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication='bbc'):
     """docstring"""
     feed = feedparser.parse(rss_feed[publication])
-    first_article = feed['entries'][0]
-    return(
-            """<html>
-                    <body>
-                        <h1>Headlines</h1>
-                        <b>{0}</b> <br/>
-                        <i>{1}</i> <br/>
-                        <p>{2}</p> <br/>
-                    </body>
-                </html>
-            """.format(first_article.get('title'),
-                       first_article.get('published'),
-                       first_article.get('summary')
-                       )
-        )
+    return render_template("home.html", articles=feed['entries'])
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
